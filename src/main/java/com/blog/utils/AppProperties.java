a8ef1dab08d85c7d08c6a8059d48883aeeb33a50
KiranMoger <kiranmgr763@gmail.com>
2026-03-19 00:19:34 +0530

    Initial commit - BlogApplication

---
+++ src/main/java/com/blog/utils/AppProperties.java
@@ -0,0 +1,9 @@
+package com.blog.utils;
+
+
+public class AppProperties 
+{
+   public static final String CLOUDINARY_CLOUD_NAME ="dxeayf5l5";
+   public static final String CLOUDINARY_API_KEY="259741777156391";
+   public static final String CLOUDINARY_SECRET_KEY="TPqSGQeu1WXWUZIxk_N012UKevE";
+}
